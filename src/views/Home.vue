c7d33ea2017dfa118a8df459580788978143acc5
qns <pale_blue_doc@163.com>
2019-10-23 16:39:08 +0800

    frist commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-aside width="200px">
+        <div class="qm-user">
+          <div class="qm-user-avatar"></div>
+        </div>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "home"
+};
 </script>
+  
+<style scoped>
+.qm-user {
+  height: 200px;
+  background-color: #3a4ba6;
+}
+</style>
